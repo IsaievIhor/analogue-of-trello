02f68df4df66e8cbe211242ccd329e40c95ce5f4
Isaev Igor <igor.igorovich.isaev@gmail.com>
2021-09-07 18:01:54 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [boards, setBoards] = useState([
+    {id:1, title:'Сделать', items:[ {id:1, title:'Пойти в магазин',},{id:2, title:'Купить вино',},{id:1, title:'Выкинуть мусор',},]},
+    {id:2, title:'Проверить', items:[ {id:4, title:'Пойти в кино',},{id:5, title:'выгулять собаку',},{id:6, title:'почистить картошку',},]},
+    {id:3, title:'Сделано', items:[ {id:7, title:'выучить реакт',},{id:8, title:'собрать миллион',},{id:9, title:'посмотреть телевизор',},]},
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {boards.map(board=>
+      <div className={'board'}>
+        <div className={'board__title'}>{board.title}</div>
+        {board.items.map(item=>
+        <div className={'item'}>{item.title}</div>
+        )}
+      </div>
+      )}
+
     </div>
   );
 }
